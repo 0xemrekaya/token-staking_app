ec6008348c6b40e53cb04e8eff4c4db79cd25414
Emre Kaya <emrekaya75@outlook.com>
2023-08-01 14:05:24 +0300

    differenct accounts and their balances

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,11 @@ class _MyHomePageState extends State<MyHomePage> {
   late Credentials credentials;
   final rpcUrl = dotenv.env['SEPOLIA_RPC_URL'];
   final privateKey = dotenv.env['PRIVATE_KEY'];
+  final privateKey2 = dotenv.env['PRIVATE_KEY2'];
+  List<Accounts> accounts = [];
+  late Accounts currentAccount;
   late Accounts account1;
+  late Accounts account2;
   BigInt? userBalance;
 
   @override
@@ -50,11 +54,15 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
   }
 
+  String getUserPrivateKey(Accounts acc) => acc.accountPrivateKey.toString();
+
   // 0x4F36Cd68f45D45a6E5574a41Bd2B9c83841c999f contract address
   Future<void> _initWeb3() async {
     httpClient = Client();
     ethClient = Web3Client(rpcUrl.toString(), httpClient);
-    credentials = EthPrivateKey.fromHex(privateKey.toString());
+    createAccount();
+    currentAccount = account2;
+    credentials = EthPrivateKey.fromHex(currentAccount.accountPrivateKey.toString());
   }
 
   Future<DeployedContract> loadContract() async {
@@ -79,29 +87,28 @@ class _MyHomePageState extends State<MyHomePage> {
     return result;
   }
 
-
   void createAccount() {
     account1 = Accounts(accountName: "Emre", accountPrivateKey: privateKey.toString());
+    account2 = Accounts(accountName: "Emre 2", accountPrivateKey: privateKey2.toString());
   }
 
-  List<Accounts> accounts = [];
-  void hesapDegistir() {
-    // Hesap değiştirme işlemleri
-    print("Hesap değiştirildi!");
+  void hesapDegistir(Accounts acc) {
+    currentAccount = acc;
+    credentials = EthPrivateKey.fromHex(currentAccount.accountPrivateKey.toString());
+    setState(() {});
+    print("Hesap değiştirildi! " + currentAccount.accountName.toString());
   }
 
   String _formatInt(BigInt balance) {
     // Convert the balance to decimal
     final balanceDecimal = balance / BigInt.from(10).pow(18);
-
     // Format the balance with desired decimal places (e.g., 2 decimal places)
     return balanceDecimal.toStringAsFixed(2);
   }
 
   @override
   Widget build(BuildContext context) {
-    createAccount();
-    accounts = [account1];
+    accounts = [account1, account2];
     return Scaffold(
         appBar: AppBar(),
         floatingActionButton: SizedBox(
@@ -117,7 +124,7 @@ class _MyHomePageState extends State<MyHomePage> {
                         title: Text(accounts[index].accountName),
                         subtitle: Text(accounts[index].accountPrivateKey.replaceAll("a", '*')),
                         onTap: () {
-                          hesapDegistir();
+                          hesapDegistir(accounts[index]);
                           Navigator.pop(context);
                         },
                       );
@@ -132,22 +139,31 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         body: Column(
           children: [
-            Center(
-              child: FutureBuilder<BigInt>(
-                future: getBalance(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(child: CircularProgressIndicator());
-                  } else if (snapshot.hasError) {
-                    return Center(child: Text('Error: ${snapshot.error.toString()}'));
-                  } else {
-                    final balance = snapshot.data;
-                    final formattedBalance = _formatInt(balance!); // Assuming 18 decimals for ETH
-
-                    return Center(child: Text('Balance of Emre token: $formattedBalance'));
-                  }
-                },
-              ),
+            FutureBuilder<BigInt>(
+              future: getBalance(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error.toString()}'));
+                } else {
+                  final balance = snapshot.data;
+                  final formattedBalance = _formatInt(balance!); // Assuming 18 decimals for ETH
+                  return Center(
+                      child: Column(
+                    children: [
+                      Text(
+                        "Current account name: ${currentAccount.accountName}",
+                        style: Theme.of(context).textTheme.headlineMedium,
+                      ),
+                      Text(
+                        'Balance of Emre token: $formattedBalance',
+                        style: Theme.of(context).textTheme.labelLarge,
+                      ),
+                    ],
+                  ));
+                }
+              },
             ),
           ],
         ));
